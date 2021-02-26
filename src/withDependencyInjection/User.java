692bcd4afca11a0cb044fa8416b2a4fd0a6b964e
Mahsa Sadi <mahsa.sadi@utoronto.edu>
2021-02-25 21:22:40 -0500

    Dependency Injection

---
+++ src/withDependencyInjection/User.java
@@ -0,0 +1,22 @@
+package withDependencyInjection;
+
+public class User {
+	
+	
+	private String name;
+	private String email;
+	
+	
+	public User ( String name, String email)
+	{
+		this.name = name;
+		this.email = email;
+	}
+	
+	
+	public String getName ()
+	{
+		return this.name;
+	}
+
+}
